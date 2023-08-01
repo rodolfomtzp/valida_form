2d19631aed31e8d7876b311b583429910a5b710a
Rodolfo <rodolfomtzp@gmail.com>
2023-08-01 14:53:23 -0600

    Mensajes de validación modificados

--- js/validaciones.js
+++ js/validaciones.js
@@ -37,34 +37,34 @@ const tipoDeErrores = [
 
 const mensajesDeError = {
     nombre : {
-        valueMissing: "Este  nombre no puede estar vacío",
+        valueMissing: "El campo nombre no puede estar vacío",
     },
     email : {
-        valueMissing: "Este campo email no puede estar vacío",
+        valueMissing: "El campo email no puede estar vacío",
         typeMismatch: "El correo no es válido",
     },
     password : {
-        valueMissing: "Este campo contaseña no puede estar vacío",
+        valueMissing: "El campo contaseña no puede estar vacío",
         patternMismatch: "Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número, no incluir caracteres especiales.",
     },
     nacimiento : {
-        valueMissing: "Este campo fecha de nacimiento no puede estar vacío",
+        valueMissing: "El campo fecha de nacimiento no puede estar vacío",
         customError: "Debes tener al menos 18 años",
     },
     telefono : {
-        valueMissing: "Este campo teléfono no puede estar vacío",
+        valueMissing: "El campo teléfono no puede estar vacío",
         patternMismatch : "El formato requerido es XXXXXXXXXX 10 números",
     },
     direccion : {
-        valueMissing: "Este campo dirección no puede estar vacío",
+        valueMissing: "El campo dirección no puede estar vacío",
         patternMismatch : "La dirección debe tener entre 10 a 40 caracteres",
     },
     ciudad : {
-        valueMissing: "Este campo ciudad no puede estar vacío",
+        valueMissing: "El campo ciudad no puede estar vacío",
         patternMismatch : "La ciudad debe tener entre 10 a 40 caracteres",
     },
     estado : {
-        valueMissing: "Este campo estado no puede estar vacío",
+        valueMissing: "El campo estado no puede estar vacío",
         patternMismatch : "El estado debe tener entre 10 a 40 caracteres",
     }
 
